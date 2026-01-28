aca8f931e3a2106249c426cf2fbf04290e8e35db
maple <14118288+gao-shengkai525866@user.noreply.gitee.com>
2026-01-28 22:07:45 +0800

    快排

--- src/main/java/com/code/hot100/sorting/QuickSort.java
+++ src/main/java/com/code/hot100/sorting/QuickSort.java
@@ -5,50 +5,49 @@ import java.util.Scanner;
 
 /**
  * @author maple
- * @Description
+ * @Description 快速排序
  * @createTime:2025-07-23 14:42
  */
 public class QuickSort {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        quickSort(arr, 0, n - 1);
-        System.out.println("Sorted Array" + Arrays.toString(arr));
+        int[] arr = {8,99,1,3,2,6,10,2,4};
+        quicksort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
-    public static void quickSort(int[] arr, int left, int right) {
-        if (left < right){
+
+    private static void quicksort(int[] arr,int left,int right) {
+        if(left < right){
             int index = partition(arr,left,right);
-            quickSort(arr,left,index-1);
-            quickSort(arr,index+1,right);
+            quicksort(arr,left,index - 1);
+            quicksort(arr,index + 1,right);
         }
     }
 
-    public static int partition(int[] arr, int left, int right) {
+    private static int partition(int[] arr,int left,int right) {
         int pivot = arr[left];
         int i = left;
         int j = right;
+
         while (i < j) {
-            while (i < j && arr[j] >= pivot) {
+            while(i < j && arr[j] >= pivot){
                 j--;
             }
-            if (i < j) {
+            if(i < j){
                 arr[i] = arr[j];
                 i++;
             }
-            while (i < j && arr[i] < pivot) {
+
+            while(i < j && arr[i] < pivot){
                 i++;
             }
-            if (i < j) {
+            if(i < j){
                 arr[j] = arr[i];
-                j++;
+                j--;
             }
         }
+
         arr[i] = pivot;
+
         return i;
     }
-
 }
